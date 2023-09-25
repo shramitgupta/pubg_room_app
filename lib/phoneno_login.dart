2099f8beaf9d320e8289cc866679ed903b9a820f
shramitgupta <shramitgupta1212@gmail.com>
2023-09-26 00:50:35 +0530

    day2

--- lib/phoneno_login.dart
+++ lib/phoneno_login.dart
@@ -13,7 +13,7 @@ class _PhonenoLoginState extends State<PhonenoLogin> {
     return Scaffold(
       body: Container(
         color: Colors.white,
-        child: const Padding(
+        child: Padding(
           padding: EdgeInsets.all(25.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
@@ -25,13 +25,60 @@ class _PhonenoLoginState extends State<PhonenoLogin> {
               ),
               Padding(
                 padding: EdgeInsets.symmetric(vertical: 20),
-                child: TextField(
+                child: TextFormField(
+                  maxLength: 10,
                   keyboardType: TextInputType.phone,
-                  decoration: InputDecoration(
+                  decoration: const InputDecoration(
+                      counter: Offstage(),
                       border: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(100))),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(100),
+                        ),
+                      ),
                       prefixIcon: Icon(Icons.phone),
                       hintText: "Phone No"),
+                ),
+              ),
+              Align(
+                alignment: Alignment.center,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style:
+                      ElevatedButton.styleFrom(backgroundColor: Colors.white),
+                  // style: ElevatedButton.styleFrom(
+                  //   backgroundColor: Colors.brown.shade900,
+                  // ),
+                  child: Text(
+                    "SEND OTP",
+                    style: TextStyle(color: Colors.brown),
+                  ),
+                ),
+              ),
+              Row(
+                //crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "New Users?",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Create an Account",
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Align(
+                alignment: Alignment.center,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(shape: StadiumBorder()),
+                  child: Text("Need Support"),
                 ),
               ),
             ],
